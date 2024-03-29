d02027678ad05c15f437cf1cdf5a14ab6e20296f
Khanbala Rashidov <50279392+KhanbalaRashidov@users.noreply.github.com>
2024-03-29 15:56:37 +0400

    Options and Head implement

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -76,5 +76,13 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return NoContent();
         }
+
+        [HttpOptions]
+        public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST, PUT, DELETE");
+
+            return Ok();
+        }
     }
 }
